f140f85cdec79cebc344e125fab5029bd40a70df
Shiny Alex <carvantes.alex@gmail.com>
2018-04-09 17:41:43 -0500

    Home-avance

--- templates/home.php
+++ templates/home.php
@@ -52,7 +52,169 @@
             </div>
         </div>
     </section>
+    
+    <section id="clientes" class="template-section">
+        <div class="contenedor">
+            <h2>Mis clientes
+           <span class="special-green"> ¡Lo confirman!</span></h2>
+            <div class="flex">
+                <div class="columna-cliente">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>../img/home/circulo-punteado.png" alt="circulo punteado">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>../img/home/cliente1.png" alt="Emma">
+                </div>
+                <div class="columna">
+                    <p class="testimonials">"Las clases de Claudia me gustan porque las sabe estructurar muy bien, es metódica y trabaja por secciones, se involucra con sus alumnas y la música es maravillosa."</p>
+                    <p class="green"><b>Emma</b></p>
+                    <p><b>- Contadora, Profesionista y Mamá. -</b></p> 
+                    
+                </div>               
+                <div class="columna-cliente">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>../img/home/circulo-punteado.png" alt="circulo punteado">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>../img/home/cliente2.png" alt="Susana">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>../img/home/circuloVerde.png" alt="circulo verde">
+                </div>
+                <div class="columna">              
+                     <p class="testimonials">"Gracias a Claus aprendí a ser consiente de lo que implica el "ejercicio", no a ir por la vida diciendo, es que voy al Gym, sino mentalizarme en voy a trabajar mi cuerpo para que siga siendo funcional muchísimo tiempo."</p>
+                    <p class="green"><b>Susana</b></p>
+                    <p><b>- M. en C. En Ingeniería Mecánica-</b></p> 
+                </div>
+            </div>
+        </div>
+    </section>
 
+   <section id="ebooks" class="template-section">
+       <div class="contenedor">
+               <div class="title-div"><h2>
+                   Mis Ebooks
+                   <span class="special-green"> y Videos </span></h2>
+               </div>
+               
+           <div class="flex">
+               <div class="columna">
+                   <div class="green-container">
+                       <img src="../img/home/libro.png" alt="COMIENZA A BRILLAR">
+                       <img src="../img/home/sombra-libro.png" alt="sombra">
+                   </div>
+               </div>
+               
+               <div class="columna">
+                   <p class="white"><b>Ebook</b></p>
+                   <p class="special-white">Hábitos Saludables</p>
+                   <p class="small"><b>Son guías que te orientarán en la mejora de tus patrones diarios (hábitos) en afán de mejorar tu calidad de vida, prevenir padecimientos crónicos y retrasar el envejecimiento prematuro.</b></p>
+                   <button class="btn-gold">¡ME INTERESA!</button>
+               </div>
+           </div>
+           
+           
+           
+           
+           <div class="flex triple">
+              <div class="columna">
+                   <div class="columna">
+                       <p class="white"><b>Ebook</b></p>
+                       <p class="special-white">Cocina Saludable</p>
+                       <p class="small"><b>Son guías que te ayudarán a organizarte para cocinar sencillo, saludable y delicioso.</b></p>
+                       <button class="btn-gold">¡ME INTERESA!</button>
+                   </div>
+               </div>
+              
+               <div class="columna">
+                   <div class="green-container">
+                       <img src="../img/home/libro.png" alt="COMIENZA A BRILLAR">
+                       <img src="../img/home/sombra-libro.png" alt="sombra">
+                   </div>
+               </div>
+               
+               <div class="columna">
+                   <img src="../img/home/transparente-verde3.png" alt="hojas">
+               </div>
+           </div>
+           
+           
+           
+           <div class="flex">
+               <div class="columna">
+                   <div class="green-container">
+                       <img src="../img/home/libro.png" alt="COMIENZA A BRILLAR">
+                       <img src="../img/home/sombra-libro.png" alt="sombra">
+                   </div>
+               </div>
+               <div class="columna">
+                   <div class="columna">
+                       <p class="white"><b>Ebook</b></p>
+                       <p class="special-white">Movimientos Saludables</p>
+                       <p class="small"><b>Son guías de apoyo para que te mantengas funcional, recuperes y conserves las cualidades físicas básicas para la vida, además de verte y sentirte muy bien.</b></p>
+                       <button class="btn-gold">¡ME INTERESA!</button>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </section>
+   
+   <section id="suscribirme">
+       <div class="contenedor">
+           <div class="flex">
+               <div class="columna">
+                   <img src="../img/home/recetario.png" alt="comidas saludables recetario">
+               </div>
+               <div class="columna">
+                   <p class="black-gray"><b>¿Te gustaría recibir avisos de mis nuevos e-books y publicaciones saludables?</b></p>
+                   <p class="green"><b>Por favor deja tu correo aqui: </b></p>
+               </div>
+           </div>
+       </div>
+   </section>
+   
+   <section id="videos">
+       <div class="contenedor">
+           <div class="flex">
+               <div class="columna">
+                   <h2>Videos de movimientos 
+                    <span class="green">saludables</span></h2>
+                   <p>Por medio de videos fáciles pero con estructura, objetivo y dosificación, te iré enseñando diferentes rutinas para recuperar, conservar o desarrollar la resistencia, fuerza, flexibilidad, equilibrio y más cualidades físicas necesarias para prevenir lesiones así como verte y sentirte muy bien.
+                   </p>
+                   <button class="btn-green">¡ME INTERESA!</button>
+               </div>
+               <div class="columna">
+                   <img src="../img/home/sun.png" alt="circulo amarillo">
+                   <img src="../img/home/sun-leaves.png" alt="adorno">
+                   <img src="../img/home/sombra-lap.png" alt="sombra de macbook">
+                   <img src="../img/home/macbook.png" alt="macbook">
+                   <img src="../img/home/pantallaMacbook.png" alt="Deliciosos y Saludables">
+               </div>
+           </div>
+       </div>
+   </section>
+   
+   <section id="coaching">
+       <div class="contenedor">
+           <div class="flex">
+              <div class="columna">
+                <h2 class="green">COACHING</h2>
+                <p>en cambio de
+                <span class="special-green">Hábitos</span></p>
+                <img src="../img/home/jugoVerde.png" alt="Jugo saludable">
+              </div>
+              <div class="columna">
+                  <p class="white">Crearé un programa  <b>totalmente personalizado,</b> basado en tus objetivos, tiempo, recursos, preferencias, gustos y limitantes.</p>
+                  <p>Es un proceso de acompañamiento personalizado en el camino de modificar diversos patrones de conducta  que tienen que ver con la salud integral, con el objetivo de adoptar gradualmente hábitos más positivos y saludables para la vida. 
+                  </p>
+                  <p class="special-white">¡Mejora tu calidad de vida y logra bienestar integral!</p>
+                  <div class="flex">
+                      <div class="columna">
+                          <img src="../img/home/circulo-punteado-amarillo.png" alt="circulo amarillo">
+                      </div>
+                      <div class="columna">
+                          <button class="btn-gold">¡ME INTERESA!</button>
+                      </div>
+                  </div>
+                  
+              </div>
+           </div>
+           <div class="flex"></div>
+       </div>
+   </section>
+   
     <?php 
     get_footer();
 ?>

5f7aed2c1843a91c3bf8ff957729d40637f154d8
Shiny Alex <carvantes.alex@gmail.com>
2018-07-19 23:33:28 -0500

    Half new section

---
+++ templates/train.php
@@ -0,0 +1,150 @@
+<?php
+    /* Template Name: Train */ 
+    get_header();
+?>
+
+    <section id="intro">
+        <?php echo do_shortcode('[rev_slider alias="principal"]'); ?>
+    </section>
+
+    <section id="entrenamiento" class="template-section padding-50">
+        <div class="contenedor">
+            <div class="pad-left flex">
+                  
+                    <h1>
+                        <b><span class="white b">ENTRENAMIENTO</span></b><br>
+                        <span class="gold m">
+                            deportivo<span class="white m"> en</span><br>
+                        </span>
+                        <span class="white">acondicionamiento</span><br>
+                        <span class="dd grass m">Físico para la vida</span>
+                    </h1>
+           
+            </div>
+        </div>
+    </section>
+
+    <section id="mejora" class="template-section">
+
+        
+        <div class="contenedor">
+           <div class="center">
+               <h2>¡Mejora tu estado físico, a tu ritmo, desde casa
+            y con total seguridad!</h2>
+           </div>
+            
+        
+
+            <p class="has-text-justified">
+                Diseñaré para ti un programa de entrenamiento personalizado que puedas realizar en casa. Tomando en cuenta tus objetivos, tiempo, recursos y limitantes. Te ayudaré a conservar y recuperar gradualmente cualidades físicas como la <b>fuerza, la flexibilidad y la resistencia,</b> entre otras, vitales para garantizar tu funcionalidad y autonomía óptima, mejorando tu calidad de vida. 
+
+            </p>
+                
+        </div>
+        <object class="divider" data="<?php echo get_stylesheet_directory_uri(); ?>/img/about/divider.svg" type="image/svg+xml"></object>
+        <div id="leave-triple"></div>
+    </section>
+    
+    
+
+    <section id="creare" class="template-section padding-50">
+        <div class="contenedor">
+             <p class=" white m dd center">
+                ¡Crearé un entrenamiento a tu medida! 
+            </p>
+        </div>
+        
+    </section>
+
+    <section id="combinando" class="template-section padding-50">
+        <div class="contenedor">
+            <div class="flex">
+                <div class="columna">
+
+                </div>
+                <div id="sqrt" class="columna">
+                    <div class="center">
+                       
+                        <p class="white pad-top">Combinando diversas metodologías (pilates, gimnasia, yoga o danza) y accesorios como: pesas ligeras, bandas de resistencia, polainas, balón suizo, arnés de suspensión, bastón, cuerda y tu propio cuerpo contra la gravedad.</p>
+
+                        <p class="white">Este programa tiene un enfoque terapéutico, correctivo y regenerativo, para que puedas autoentrenarte con seguridad durante un mes, sin lesionarte y logrando adaptación para poder modificar y progresar en el entrenamiento.</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+         
+    </section>
+    
+   
+
+    <section id="videos-home" class="padding-50 template-section">
+        <div class="contenedor">
+            <div class="flex">
+                <div class="columna">
+                    <h2>Videos de movimientos
+                        <span class="green">saludables</span></h2>
+                    <p>Por medio de videos fáciles pero con estructura, objetivo y dosificación, te iré enseñando diferentes rutinas para recuperar, conservar o desarrollar la resistencia, fuerza, flexibilidad, equilibrio y más cualidades físicas necesarias para prevenir lesiones así como verte y sentirte muy bien.
+                    </p>
+                    <a class="boton verde">¡ME INTERESA!</a>
+                </div>
+                <div class="columna">
+                    <img class="videos" src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/videos.png" alt="circulo amarillo">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="coaching-home" class="template-section">
+        <div class="contenedor">
+            <div class="flex">
+                <div class="columna">
+                    <h2 class="green">COACHING
+                        <span class="big-green">en cambio de
+                        <b class="special-green">Hábitos</b></span></h2>
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home/jugoVerde.png" alt="Jugo saludable">
+                </div>
+                <div class="columna">
+                    <p class="white">Crearé un programa <b>totalmente personalizado,</b> basado en tus objetivos, tiempo, recursos, preferencias, gustos y limitantes.</p>
+                    <p>Es un proceso de acompañamiento personalizado en el camino de modificar diversos patrones de conducta que tienen que ver con la salud integral, con el objetivo de adoptar gradualmente hábitos más positivos y saludables para la vida.
+                    </p>
+                    <p class="cursiva">¡Mejora tu calidad de vida y logra bienestar integral!</p>
+                    <a class="boton naranja-dos">¡ME INTERESA!</a>
+
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section id="entrenamiento-home" class="template-section">
+        <div class="contenedor">
+            <div class="flex">
+                <div class="columna">
+                    <h2>ENTRENAMIENTO
+                        <span class="big-green">deportivo en</span>
+                        <span class="acondicionamiento">acondicionamiento</span>
+                        <span class="cursiva">[Físico para la vida]</span></h2>
+
+
+                    <div class="info">
+                        <p>Diseñaré para ti un programa de entrenamiento personalizado que puedas realizar en casa. Tomando en cuenta tus objetivos, tiempo, recursos y limitantes.
+                        </p>
+                        <p>Te ayudaré a conservar y recuperar gradualmente cualidades físicas como <b>la fuerza, la flexibilidad y la resistencia</b>, entre otras, vitales para garantizar tu funcionalidad y autonomía óptima, mejorando tu calidad de vida. </p>
+                        <p class="cursiva">Mejora estado físico, desde tu casa y a tu ritmo! </p>
+                        <a class="boton naranja">¡ME INTERESA!</a>
+                    </div>
+                </div>
+                <div class="columna">
+
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+
+    <?php 
+    get_footer();
+?>
